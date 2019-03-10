5843bc0311f9b292015929ff2499b25b221a27fc
Kat Keller <kat.kek.keller@gmail.com>
2019-03-10 16:53:13 -0500

    Added a score counter and a strike counter

--- Experimental Game/Assets/Scripts/AnswerPicker.cs
+++ Experimental Game/Assets/Scripts/AnswerPicker.cs
@@ -12,9 +12,17 @@ public class AnswerPicker : MonoBehaviour
     private Text catagoryText;
 
     [SerializeField]
-    private Button continueButton;
+    private Button skipButton;
+
+    [SerializeField]
+    private Text scoreText;
+
+    [SerializeField]
+    private Text strikeText;
 
     private string answer = "Answer";
+    private int numberOfStrikes = 0;
+    private int score = 0;
 
     private string[] danceAnswers = new string[] { "Macarena", "Cupid Shuffle", "Electric Slide", "Cotton Eye Joe", "The Worm", "Soulja Boy", "The Chicken Dance", "Twerking", "The Moon Walk", "Voguing", "The Twist", "Disco", "The YMCA", "Head Banging", "Thriller", "The Robot", "Gangnam Style", "What is Love", "Bye Bye Bye" };
 
@@ -28,6 +36,13 @@ public class AnswerPicker : MonoBehaviour
     public string GetRandomSportAnswer()
     {
         return sportAnswers[Random.Range(0, sportAnswers.Length)];
+    }
+
+    private void Start()
+    {
+        SetCurrentAnswer();
+        scoreText.text = "0";
+        strikeText.text = "";
     }
 
     public void SetCurrentAnswer()
@@ -44,5 +59,29 @@ public class AnswerPicker : MonoBehaviour
             answerText.text = answer;
             catagoryText.text = "Sports";
         }
+
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    public void Skip()
+    {
+        numberOfStrikes++;
+
+        if (numberOfStrikes == 1)
+        {
+            strikeText.text = "X";
+        }
+        else if (numberOfStrikes == 2)
+        {
+            strikeText.text = "X X";
+        }
+        else if (numberOfStrikes == 3)
+        {
+            strikeText.text = "X X X";
+            skipButton.enabled = false;
+        }
+
+        SetCurrentAnswer();
     }
 }
