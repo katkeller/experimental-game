ab467243bc6e3329e83e1d0fb336a10eb6c22dba
Kat Keller <kat.kek.keller@gmail.com>
2019-03-10 15:01:53 -0500

    Added ColorChanging script and added it to the background

---
+++ Experimental Game/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorChanger : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1.0f;
+
+    [SerializeField]
+    private Color startColor;
+
+    [SerializeField]
+    private Color endColor;
+
+    [SerializeField]
+    private Image background;
+
+    [SerializeField]
+    private bool repeatable = false;
+
+    private float startTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (!repeatable)
+        {
+            float t = (Time.time - startTime) * speed;
+            background.color = Color.Lerp(startColor, endColor, t);
+        }
+        else
+        {
+            float t = (Mathf.Sin(Time.time - startTime) * speed);
+            background.color = Color.Lerp(startColor, endColor, t);
+        }
+    }
+}
